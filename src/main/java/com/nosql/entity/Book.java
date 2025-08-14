295e4901e11408781a928e8fc2227849d035a8e7
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-08-14 15:54:09 +0500

    Defining the Book Entity Class

---
+++ src/main/java/com/nosql/entity/Book.java
@@ -0,0 +1,63 @@
+package com.nosql.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "books")
+public class Book {
+    @Id
+    private String id;
+    private String title;
+    private String author;
+    private double price;
+    private String genre;
+
+    // Constructors, getters, and setters
+
+    public Book(String title, String author, double price, String genre) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.genre = genre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
