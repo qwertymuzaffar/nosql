678d4ed10267bd7770d43b5c2d7e9315d36f21fa
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-08-14 15:42:32 +0500

    Configuring MongoDB

--- src/main/java/com/nosql/NosqlApplication.java
+++ src/main/java/com/nosql/NosqlApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NosqlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NosqlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NosqlApplication.class, args);
+    }
 
 }
--- src/test/java/com/nosql/NosqlApplicationTests.java
+++ src/test/java/com/nosql/NosqlApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NosqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
